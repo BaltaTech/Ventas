caef1dde8092567d60a7d0e55f81222e721af442
Saul Baltazar <chrisnxtch@gmail.com>
2026-05-18 16:18:34 -0600

    Refactor AutoMapper registration and service injection
    Removed comments about manual AutoMapper registration and ensured the registration of services is consistent.

--- Application/InyeccionDependencias.cs
+++ Application/InyeccionDependencias.cs
@@ -9,15 +9,13 @@ public static class InyeccionDependencias
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // Esta es la forma manual de registrar AutoMapper en las versiones nuevas
-        // sin depender del paquete de "DependencyInjection" que causaba conflictos.
         services.AddAutoMapper(cfg =>
         {
-            cfg.AddProfile<MappingProfile>(); // Reemplaza 'MappingProfile' por el nombre de tu clase de perfiles
+            cfg.AddProfile<MappingProfile>(); 
         }, Assembly.GetExecutingAssembly());
 
         services.AddScoped<IProductoService, ProductoService>();
-        services.AddScoped<IProspectoService, ProspectoService>(); // <--- Esto ya no debería marcar error
+        services.AddScoped<IProspectoService, ProspectoService>(); 
 
         return services;
     }
